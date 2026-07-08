253d4f89d81a16cbb9d2fb06a16bb4f0c7d59e29
CHANDANAcs@02 <chandanacs2004@gmail.com>
2026-07-08 07:26:02 +0530

    [Chandana] : Added capacity validation to Bogie constructor

--- src/Bogie.java
+++ src/Bogie.java
@@ -8,7 +8,13 @@ public class Bogie {
     private int capacity;
 
     // Constructor
-    public Bogie(String name, int capacity) {
+    public Bogie(String name, int capacity) throws InvalidCapacityException {
+
+        if (capacity <= 0) {
+            throw new InvalidCapacityException(
+                    "Capacity must be greater than zero");
+        }
+
         this.name = name;
         this.capacity = capacity;
     }
