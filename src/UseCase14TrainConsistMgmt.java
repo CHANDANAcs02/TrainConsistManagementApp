f124a7330231a3546aaab6d7162edd1378f0e201
CHANDANAcs@02 <chandanacs2004@gmail.com>
2026-07-08 07:26:08 +0530

    [Chandana] : Completed UC14 Invalid bogie capacity handling

--- src/UseCase14TrainConsistMgmt.java
+++ src/UseCase14TrainConsistMgmt.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
-
+/**
+ * ==========================================================
+ * MAIN CLASS - UseCase14TrainConsistMgmt
+ * ==========================================================
+ *
+ * Use Case 14:
+ * Handle Invalid Bogie Capacity using
+ * a Custom Exception.
+ */
 public class UseCase14TrainConsistMgmt {
+
+    public static void main(String[] args) {
+
+        System.out.println("========================================");
+        System.out.println(" UC14 - Handle Invalid Bogie Capacity ");
+        System.out.println("========================================\n");
+
+        try {
+
+            Bogie bogie = new Bogie("Sleeper", 72);
+            System.out.println("Created Bogie: " + bogie);
+
+            // Invalid Bogie
+            Bogie invalid = new Bogie("AC Chair", -20);
+
+            System.out.println(invalid);
+
+        } catch (InvalidCapacityException e) {
+
+            System.out.println("Error: " + e.getMessage());
+
+        }
+
+        System.out.println("\nUC14 exception handling completed...");
+    }
 }
