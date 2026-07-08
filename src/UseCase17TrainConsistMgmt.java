87368e087447555637cb4d50e576ef3149978977
CHANDANAcs@02 <chandanacs2004@gmail.com>
2026-07-08 07:34:44 +0530

    [Chandana] : Completed UC17 Sorting bogie names using Arrays.sort()

---
+++ src/UseCase17TrainConsistMgmt.java
@@ -0,0 +1,42 @@
+import java.util.Arrays;
+
+/**
+ * ==========================================================
+ * MAIN CLASS - UseCase17TrainConsistMgmt
+ * ==========================================================
+ *
+ * Use Case 17:
+ * Sort Bogie Names using Arrays.sort().
+ *
+ * Concepts:
+ * - Arrays.sort()
+ * - Arrays.toString()
+ * - Natural Ordering
+ */
+public class UseCase17TrainConsistMgmt {
+
+    public static void main(String[] args) {
+
+        System.out.println("==================================================");
+        System.out.println(" UC17 - Sort Bogie Names Using Arrays.sort() ");
+        System.out.println("==================================================\n");
+
+        String[] bogieNames = {
+                "Sleeper",
+                "AC Chair",
+                "First Class",
+                "General",
+                "Luxury"
+        };
+
+        System.out.println("Original Bogie Names:");
+        System.out.println(Arrays.toString(bogieNames));
+
+        Arrays.sort(bogieNames);
+
+        System.out.println("\nSorted Bogie Names (Alphabetical):");
+        System.out.println(Arrays.toString(bogieNames));
+
+        System.out.println("\nUC17 sorting completed...");
+    }
+}
